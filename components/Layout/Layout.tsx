b9d06e5788133b427430c51e15795300f7ab9078
Matthew Mercuri <mercurimatthew@gmail.com>
2022-11-09 20:56:12 -0500

    added typescript eslint config

--- components/Layout/Layout.tsx
+++ components/Layout/Layout.tsx
@@ -2,7 +2,7 @@ type LayoutProps = {
   children: React.ReactNode;
 };
 
-const Layout: React.FC<LayoutProps> = () => {
+const Layout: React.FC<LayoutProps> = ({ children }) => {
   return <div />;
 };
 
