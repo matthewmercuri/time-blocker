d6bb370009758ce4db1057094040d79e38740d34
Matthew Mercuri <mercurimatthew@gmail.com>
2022-11-09 21:14:44 -0500

    added sass

--- next.config.js
+++ next.config.js
@@ -1,7 +1,12 @@
+import path from 'path';
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-}
+  sassOptions: {
+    includePaths: [path.join(__dirname, 'styles')],
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
